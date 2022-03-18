ce573ad9ba13d09458f9e6d31767e2b471b12557
Miloshevich George <george.miloshevich@ens-lyon.fr>
2022-03-18 16:58:34 +0100

    now VAE/history.py plots the history for all folds

--- VAE/history.py
+++ VAE/history.py
@@ -6,6 +6,9 @@ import numpy as np
 import os
 import sys
 import matplotlib.pyplot as plt
+nfolds = int(sys.argv[2])
+
+# we must now number of folds in advance
 
 def plot_metric(i,train_metrics, val_metrics, name_metrics,color_idx):
     epochs = range(1, len(train_metrics) + 1)
@@ -20,23 +23,24 @@ def plot_metric(i,train_metrics, val_metrics, name_metrics,color_idx):
     plt.ylabel('metrics')
     return epochs
 
-checkpoint_name = sys.argv[1]
+folder = sys.argv[1]
 
 fig = plt.figure()
-history = np.load(checkpoint_name+'/history_vae', allow_pickle=True)#.item()
-print(history.keys())
+for i in range(nfolds):
+    history = np.load(f'{folder}/fold_{i}/history_vae', allow_pickle=True)#.item()
+    print(history.keys())
 
-for key in history:
-    epochs = range(1, len(history[key])+1)
-    print(epochs)
-    plt.plot(epochs, history[key], label =key)
-#loss.append(history[metric_name])
-#val_loss.append(history['val_'+metric_name])
- #   epochs = plot_metric(i,history[metric_name], history['val_'+metric_name], metric_name, color_idx[i])
+    for key in history:
+        epochs = range(1, len(history[key])+1)
+        print(epochs)
+        plt.plot(epochs, history[key], label =f'{key}_{i}')
+    #loss.append(history[metric_name])
+    #val_loss.append(history['val_'+metric_name])
+     #   epochs = plot_metric(i,history[metric_name], history['val_'+metric_name], metric_name, color_idx[i])
 
-#loss = np.array(loss)
-#plt.plot(epochs, np.mean(loss,0), 'g--', linewidth = 2, label ='train average')
-#plt.plot(epochs, np.mean(val_loss,0), 'g-.', linewidth = 2, label ='test average')
+    #loss = np.array(loss)
+    #plt.plot(epochs, np.mean(loss,0), 'g--', linewidth = 2, label ='train average')
+    #plt.plot(epochs, np.mean(val_loss,0), 'g-.', linewidth = 2, label ='test average')
 
 plt.legend(loc='best')
 plt.show()
