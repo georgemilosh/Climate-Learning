24497c60ecb47540f94db541b3b9250304dcad8c
Miloshevich George <george.miloshevich@ens-lyon.fr>
2022-04-07 20:08:03 +0200

    Added kNN classifier and a corresponding method key, score ipynb has to be updated correspondingly

--- VAE/classification2.py
+++ VAE/classification2.py
@@ -41,6 +41,7 @@ import random as rd
 from scipy.stats import norm
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn import neighbors
 
 tff = foo.tff # tensorflow routines 
 ut = foo.ut # utilities
@@ -93,7 +94,13 @@ def classify(fold_folder, evaluate_epoch, vae, X_tr, z_tr, Y_tr, X_va, z_va, Y_v
         checkpoints = list(range(0,evaluate_epoch+1,10))
     checkpoints[0] = 1
     score = []
+    labels = ['Log_L2','kNN_uni','kNN_dst']
+    L_parameter = [1e-3, 1e-2, 5e-2, 1e-1, 5e-1, 1e0, 1e1, 1e2, 1e3, 1e5,1e7,1e9] # Logistic regularization coefficients (L2 by default)
+    K_parameter = [1, 2, 3, 5, 10, 15, 20, 40, 100, 200, 400, 800, 1600] # Number of KNN
+    #K_parameter2 = [1, 2, 3, 5, 10, 15, 20, 40, 100] # Number of KNN
+
     for checkpoint in checkpoints:
+        score_method = []
         checkpoint_path = str(fold_folder)+f"/cp_vae-{checkpoint:04d}.ckpt" # TODO: convert checkpoints to f-strings
         logger.info(f"==loading the model: {checkpoint_path}")
         vae = tf.keras.models.load_model(fold_folder, compile=False)
@@ -106,20 +113,25 @@ def classify(fold_folder, evaluate_epoch, vae, X_tr, z_tr, Y_tr, X_va, z_va, Y_v
         logger.info(f"Before undersampling: {len(Y_tr) = }, {len(Y_va) = }, {np.sum(Y_tr==1) = }, {np.sum(Y_va==1) = }")    
         z_tr, Y_tr = ln.undersample(z_tr, Y_tr, u=u)  
         logger.info(f"After undersampling: {len(Y_tr) = }, {len(Y_va) = }, {np.sum(Y_tr==1) = }, {np.sum(Y_va==1) = }")    
-        C_parameter = [1e-3, 1e-2, 5e-2, 1e-1, 5e-1, 1e0, 1e1, 1e2, 1e3, 1e5,1e7,1e9] # Different regularization coefficients (L2 by default)
-        classifier = [LogisticRegression(solver='liblinear',C=index_i) for index_i in C_parameter]
-        TP, TN, FP, FN, MCC, entropy, skill, BS, freq = [np.zeros(len(classifier),) for x in range(9)]
-        for i in range(len(classifier)):
-            classifier[i].fit(z_tr, Y_tr)
-            Y_pr = classifier[i].predict(z_va) 
-            Y_pr_prob = classifier[i].predict_proba(z_va)
+        
+        logisticclassifier = [LogisticRegression(solver='liblinear',C=index_i) for index_i in L_parameter]
+        kNNclassifier = [neighbors.KNeighborsClassifier(n_neighbors, weights="uniform") for n_neighbors in K_parameter]
+        kNNclassifier2 = [neighbors.KNeighborsClassifier(n_neighbors, weights="distance") for n_neighbors in K_parameter]
+        for classifier, C_parameter in zip([logisticclassifier, kNNclassifier,kNNclassifier2], [L_parameter,K_parameter,K_parameter]):
+            logger.info(f"{classifier = }")
+            TP, TN, FP, FN, MCC, entropy, skill, BS, freq = [np.zeros(len(classifier),) for x in range(9)]
+            for i in range(len(classifier)):
+                classifier[i].fit(z_tr, Y_tr)
+                Y_pr = classifier[i].predict(z_va) 
+                Y_pr_prob = classifier[i].predict_proba(z_va)
 
-            TP[i], TN[i], FP[i], FN[i], MCC[i] = ef.ComputeMCC(Y_va, Y_pr, 'True')
-            _, entropy[i], skill[i], BS[i], _, freq[i] = ef.ComputeMetrics(np.array(Y_va), Y_pr_prob, percent, reundersampling_factor=u, print_output=False) 
-            
-            #logger.info(f"{Y_pr[23] = }") # Just testing if data is processed properly (potentially remove this line)
-        score.append(pd.DataFrame(np.array([C_parameter, TP, TN, FP, FN, MCC, entropy, skill, BS, freq]).transpose(), columns =['C','TP', 'TN', 'FP', 'FN', 'MCC', 'entropy', 'skill','Brier','freq'])) 
-    score = pd.concat(score, keys=checkpoints,names=['checkpoint', None])
+                TP[i], TN[i], FP[i], FN[i], MCC[i] = ef.ComputeMCC(Y_va, Y_pr, 'True')
+                _, entropy[i], skill[i], BS[i], _, freq[i] = ef.ComputeMetrics(np.array(Y_va), Y_pr_prob, percent, reundersampling_factor=u, print_output=False) 
+
+            score_method.append(pd.DataFrame(np.array([C_parameter, TP, TN, FP, FN, MCC, entropy, skill, BS, freq]).transpose(), columns =['C','TP', 'TN', 'FP', 'FN', 'MCC', 'entropy', 'skill','Brier','freq'])) 
+        score.append(pd.concat(score_method, keys=labels,names=['method', None]))
+
+    score = pd.concat(score, keys=checkpoints,names=['checkpoint','method', None])
     logger.info('score:')
     logger.info(f'{score}')
     logger.info(f"{Style.RESET_ALL}")
@@ -137,7 +149,7 @@ logger.info(f"{Fore.BLUE}") #  indicates we are inside the routine
 # the rest of the code goes here
 #logger.info('score = ')
 #logger.info(f'{score}')
-score = pd.concat(score, keys=range(len(score)),names=['fold','checkpoint', None])
+score = pd.concat(score, keys=range(len(score)),names=['fold','checkpoint','method', None])
 #logger.info('score:')
 #logger.info(f'{score}')
 score.to_csv(f'{folder}/score.csv')
