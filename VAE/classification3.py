83eef36b181cc06e81ba46f19304da517b4147dd
Miloshevich George <george.miloshevich@ens-lyon.fr>
2022-05-10 11:28:29 +0200

    Now classification3 checks what kind of input was used in vae.classifier (i.e. stochastic or not)

--- VAE/classification3.py
+++ VAE/classification3.py
@@ -85,6 +85,12 @@ def classify(fold_folder, evaluate_epoch, vae, X_tr, z_tr, Y_tr, X_va, z_va, Y_v
     logger.info(f"{vae = }, {fold_folder = }")
     u=1 #10
     percent = ut.extract_nested(run_vae_kwargs, 'percent')
+    from_logits = ut.extract_nested(run_vae_kwargs, 'from_logits')
+    if ut.keys_exists(run_vae_kwargs, 'class_type'):
+        class_type = ut.extract_nested(run_vae_kwargs, 'class_type')
+    else:
+        class_type = None
+    
     logger.info(f"{Fore.BLUE}")
     logger.info(f"==classify of classification.py==")
     #logger.info(f"{X_va[23,15,65,0] = }, {z_va[23,14] = }, {Y_va[23] = }") # Just testing if data is processed properly (potentially remove this line)
@@ -99,7 +105,7 @@ def classify(fold_folder, evaluate_epoch, vae, X_tr, z_tr, Y_tr, X_va, z_va, Y_v
         checkpoints = list(range(0,evaluate_epoch+1,1))
     checkpoints = checkpoints[1:]
     score = []
-    labels = ['Log_L2','kNN_uni','vae.class']
+    labels = ['Log_L2','Log_L1','kNN_uni','vae.class']
     L_parameter = [1e-5,1e-3, 1e-2, 1e-1, 5e-1, 1e0, 1e1, 1e2, 1e3, 1e5] # Logistic regularization coefficients (L2 by default)
     K_parameter = [40, 75, 100, 150, 200, 400, 800, 1600] # Number of KNN
     #K_parameter2 = [1, 2, 3, 5, 10, 15, 20, 40, 100] # Number of KNN
@@ -117,11 +123,11 @@ def classify(fold_folder, evaluate_epoch, vae, X_tr, z_tr, Y_tr, X_va, z_va, Y_v
             else:
                 found = 1
                 logger.info(f"==loading the model: {checkpoint_path}")
-                vae = tf.keras.models.load_model(fold_folder, compile=False)
+                # vae = tf.keras.models.load_model(fold_folder, compile=False) # i commented this because vae is already supplied as the input to the classifier
                 vae.load_weights(f'{checkpoint_path}').expect_partial()
                 logger.info(f'{checkpoint_path} weights loaded')
                 _,_,z_tr = vae.encoder.predict(X_tr)
-                _,_,z_va = vae.encoder.predict(X_va)
+                z_mean_va,_,z_va = vae.encoder.predict(X_va)
                 logger.info(f"{z_tr.shape = }, {z_va.shape = }" )
 
                 logger.info(f"Before undersampling: {len(Y_tr) = }, {len(Y_va) = }, {np.sum(Y_tr==1) = }, {np.sum(Y_va==1) = }")    
@@ -129,17 +135,28 @@ def classify(fold_folder, evaluate_epoch, vae, X_tr, z_tr, Y_tr, X_va, z_va, Y_v
                 logger.info(f"After undersampling: {len(Y_tr) = }, {len(Y_va) = }, {np.sum(Y_tr==1) = }, {np.sum(Y_va==1) = }")    
                 maxskill = -(percent/100.)*np.log(percent/100.)-(1-percent/100.)*np.log(1-percent/100.)
                 logisticclassifier = [LogisticRegression(solver='liblinear',C=index_i) for index_i in L_parameter]
+                lassoclassifier = [LogisticRegression(solver='liblinear', penalty="l1", C=index_i) for index_i in L_parameter]
                 kNNclassifier = [neighbors.KNeighborsClassifier(n_neighbors, weights="uniform",n_jobs=32) for n_neighbors in K_parameter]
                 vaeclassifier = ['vae.classifier']
-                for classifier, C_parameter in zip([logisticclassifier, kNNclassifier,vaeclassifier], [L_parameter,K_parameter,[0]]):
+                for classifier, C_parameter in zip([logisticclassifier, lassoclassifier, kNNclassifier,vaeclassifier], [L_parameter,L_parameter,K_parameter,[0]]):
                     logger.info(f"{classifier = }")
                     entropy, skill= [np.zeros(len(classifier),) for x in range(2)]
                     for i in range(len(classifier)):
                         if (classifier[i] == 'vae.classifier'):
                             if hasattr(vae, 'classifier'):
                                 logger.info("vae.classifier fit")
-                                Y_pr_prob = vae.classifier.predict(z_va)
+                                if class_type is not None:
+                                    if class_type == "stochastic":
+                                        logger.info("vae.classifier is stochastic")
+                                        Y_pr_prob = vae.classifier.predict(z_va)
+                                    else: # i.e. "mean"
+                                        logger.info("vae.classifier is deterministic")
+                                        Y_pr_prob = vae.classifier.predict(z_mean_va)
+                                else:
+                                    logger.info("vae.classifier is stochastic")
+                                    Y_pr_prob = vae.classifier.predict(z_va)
                             else:
+                                logger.info("vae.classifier not available, setting prediction to zero")
                                 Y_pr_prob = np.zeros(z_va.shape)
                         else: # we are running sklearn classifiers
                             classifier[i].fit(z_tr, Y_tr)
@@ -148,7 +165,10 @@ def classify(fold_folder, evaluate_epoch, vae, X_tr, z_tr, Y_tr, X_va, z_va, Y_v
                             Y_pr_prob = (classifier[i].predict_proba(z_va))[:, 1]
                         
                         logger.info("model predict")
-                        entropy[i] = log_loss(Y_va, Y_pr_prob)
+                        if classifier[i] == 'vae.classifier':
+                            entropy[i] = tf.keras.losses.BinaryCrossentropy(from_logits=from_logits)(Y_va,Y_pr_prob)  # Here we rely on tensorflow binary cross entropy that was used to compute probabilities
+                        else:
+                            entropy[i] = log_loss(Y_va, Y_pr_prob) # Here we rely on sklearn
                         skill[i] = (maxskill-entropy[i])/maxskill
                         logger.info(f"{skill.shape = }")
                         # TP[i], TN[i], FP[i], FN[i], MCC[i] = ef.ComputeMCC(Y_va, Y_pr, 'True')
