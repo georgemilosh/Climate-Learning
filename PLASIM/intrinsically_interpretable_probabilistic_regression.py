446337776ea661879c1b6fd144dc73aab8c02ec7
AlessandroLovo <ale571.lovo@gmail.com>
2024-01-11 19:11:57 +0100

    #82: first attempt with IIPR

--- PLASIM/intrinsically_interpretable_probabilistic_regression.py
+++ PLASIM/intrinsically_interpretable_probabilistic_regression.py
@@ -22,28 +22,7 @@ if __name__ == '__main__':
     logging.getLogger().level = logging.INFO
     logging.getLogger().handlers = [logging.StreamHandler(sys.stdout)]
 
-# pr.Trainer overwrote the prepate_XY mod of iin that saved the lat variable. We need to restore it
-class Trainer(ln.Trainer):
-    def prepare_XY(self, fields, **prepare_XY_kwargs):
-        res =  super().prepare_XY(fields, **prepare_XY_kwargs)
-        logger.info('Saving latitude as module level variable')
-        ln.lat = self.lat
-        return res
-    
-ln.Trainer = Trainer
-ln.CONFIG_DICT = ln.build_config_dict([ln.Trainer.run, ln.Trainer.telegram]) # module level config dictionary
-# now that we rebuilt the config dictionary, we have to set the default values of pr again
-# TODO: improve this
-
-ut.set_values_recursive(ln.CONFIG_DICT,
-                        {
-                            'return_threshold': True,
-                            'loss': 'crps',
-                            'return_metric': 'val_ParametricCrossEntropyLoss',
-                            'monitor' : 'val_ParametricCrossEntropyLoss',
-                            'metric' : 'val_ParametricCrossEntropyLoss',
-                        },
-                        inplace=True) 
+## we don't have to add anything as pr and iinn are already imported and will combine well
 
 if __name__ == '__main__':
     ln.main()
