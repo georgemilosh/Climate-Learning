db073de1d52ac6b99937dd6c863863247b0c1027
AlessandroLovoENSL <alessandro.lovo@ens-lyon.fr>
2024-01-29 18:18:46 +0100

    Addressing #92 for import_config.py

--- PLASIM/import_config.py
+++ PLASIM/import_config.py
@@ -4,6 +4,11 @@ Imports the argument from a given config file to the one in the current director
 Usage:
     python import_config.py <path to config file from which to import arguments>
 
+Examples:
+    python import_config.py ../t1/config.json  # imports arguments from ../t1/config.json
+    python import_config.py ../t1              # imports arguments from ../t1/config.json
+    python import_config.py ../t1/43           # imports arguments from the parameters of run 43 in ../t1, i.e. both the config file ../t1/config.json and the non-default paraamters specified by run 43
+
 If the two config files have different arguments it is not a problem since the importation will affect only the arguments in common between the two.
 '''
 # GM: perhaps it would be nice to explain reasons for why this might be necessary
@@ -25,14 +30,29 @@ if not os.access('./config.json', os.W_OK):
 
 config = ut.json2dict('./config.json')
 
-path_to_import_config = sys.argv[1]
-if not path_to_import_config.endswith('config.json'):
-    path_to_import_config = path_to_import_config.rstrip('/')
-    path_to_import_config += '/config.json'
+run_id=None
+path_to_import_config = Path(sys.argv[1])
+if not path_to_import_config.stem == 'config':
+    # maybe is just missing a 'config.json'
+    if (path_to_import_config / 'config.json').exists():
+        path_to_import_config = path_to_import_config / 'config.json'
+    elif (path_to_import_config.parent / 'config.json').exists():
+        try:
+            run_id = f'{int(path_to_import_config.stem)}'
+        except ValueError:
+            raise ValueError('run_id must be the string representation of an integer')
+        path_to_import_config = path_to_import_config.parent / 'config.json'
+    else:
+        raise FileNotFoundError('There is no config file in this directory')
 
 config_to_import = ut.json2dict(path_to_import_config)
 config_to_import_flat = ut.collapse_dict(config_to_import)
 
-config = ut.set_values_recursive(config, config_to_import_flat)
+ut.set_values_recursive(config, config_to_import_flat, inplace=True)
+
+if run_id is not None:
+    runs = ut.json2dict(path_to_import_config.parent / 'runs.json')
+    run = runs[run_id]
+    ut.set_values_recursive(config, run['args'], inplace=True)
 
 ut.dict2json(config, './config.json')
